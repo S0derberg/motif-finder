c703d7d3d086423ea126b7af93fcc7f7109329f3
S0derberg <sidneyo2@illinois.edu>
2018-05-02 16:52:23 -0500

    just did beam search

--- beamSearch.py
+++ beamSearch.py
@@ -6,22 +6,55 @@ import utilities as utils
 import sequenceReader as reader
 
 """
-	File Name: greedySearch.py
+	File Name: beamSearch.py
 	Author: Sidney Oderberg
-	Description: This file implements the Greedy Search algorithm for motif finding
+	Description: This file implements the Beam Search algorithm for motif finding
 """
 
 L = 8
+K = 10
 BASE_DICT = {0: "A", 1: "C", 2: "G", 3: "T"}
 
+# A data structure to keep track of the best K searches
+class K_Best:
+	def __init__(self, k):
+		self.k = k
+		self.data = {}
+		self.worst = -1
 
-# Run the greedy search on the DNA sequence list
+		for i in range(k):
+			self.data[i] = (-1, None)
+
+
+	def add(self, value, data):
+		if value <= self.worst:
+			return
+
+		node = (value, data)
+		self.data[self.k-1] = node
+
+		i = self.k-2
+		while i >= 0:
+			if value > self.data[i][0]:
+				self.data[i+1] = self.data[i]
+				self.data[i] = node
+				i -= 1
+			else:
+				break
+		self.worst = self.data[self.k-1][0]
+
+	def get_data(self):
+		return self.data
+
+
+
+# Run the beam search on the DNA sequence list
 def main(args):
 
 	print("Loading DNA sequences.")
 	sequenceList = reader.read("data/hm01r.fasta")
 
-	print("Beginning Greedy Algorithm for Motif Finding")
+	print("Beginning Beam Search Algorithm for Motif Finding")
 	print("    Motif Length: {}".format(L))
 	print("    Number of Sequences: {}".format(len(sequenceList)))
 
@@ -31,6 +64,7 @@ def main(args):
 	seq_2 = sequenceList[1]
 	highest_IC = 0
 	best_motif_list = []
+	best_k = K_Best(K)
 
 	base_probs = utils.base_probabilities(sequenceList)
 
@@ -44,29 +78,37 @@ def main(args):
 			PWM = utils.position_weight_matrix(PM, 2)
 			IC = utils.information_content(PWM, base_probs, L)
 			
-			if IC > highest_IC:
-				highest_IC = IC
-				best_motif_list = [motif_1, motif_2]
+			best_k.add(IC, [motif_1, motif_2])
 
+
+	prev_best_k = best_k
 	
 	for k in range(len(sequenceList)-2):
 		curr_seq = sequenceList[k+2]
-		highest_IC = 0
-		best_motif = None
+		# highest_IC = 0
+		# best_motif = None
+		curr_best_k = K_Best(K)
 
 		for p in range(len(curr_seq)-L+1):
 			motif_k = curr_seq[p:p+L]
 
-			PM = utils.profile_matrix(best_motif_list + [motif_k], L)
-			PWM = utils.position_weight_matrix(PM, k+3)
-			IC = utils.information_content(PWM, base_probs, L)
+			for t in range(K):
+				motif_list = prev_best_k.get_data()[t][1] + [motif_k]
 
-			if IC > highest_IC:
-				highest_IC = IC
-				best_motif = motif_k
+				PM = utils.profile_matrix(motif_list, L)
+				PWM = utils.position_weight_matrix(PM, k+3)
+				IC = utils.information_content(PWM, base_probs, L)
 
-		best_motif_list.append(best_motif)
+				# if IC > highest_IC:
+				# 	highest_IC = IC
+				# 	best_motif = motif_k
+				curr_best_k.add(IC, motif_list)
 
+		# best_motif_list.append(best_motif)
+		prev_best_k = curr_best_k
+
+
+	best_motif_list = prev_best_k.get_data()[0][1]
 
 	final_PM = utils.profile_matrix(best_motif_list, L)
 	final_PWM = utils.position_weight_matrix(PM, len(sequenceList))
@@ -76,7 +118,7 @@ def main(args):
 
 	elapsed_time = time.time() - start_time
 
-	print("\nGreedy Search Complete.")
+	print("\nBeam Search Complete.")
 	print("    Motif Found: " + consensus_motif)
 	print("    Information Content: {}".format(final_IC))
 	print("    Elapsed Time: {} seconds".format(elapsed_time))
@@ -90,3 +132,5 @@ def main(args):
 
 if __name__=="__main__":
 	main(sys.argv)
+
+
